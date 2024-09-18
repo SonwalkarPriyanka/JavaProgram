e10947645e5a73de78b4625740f1e383419fac88
SonwalkarPriyanka <147388032+SonwalkarPriyanka@users.noreply.github.com>
2024-09-18 22:23:10 +0530

    First Commit

---
+++ Git/src/Gitt/GitHello.java
@@ -0,0 +1,12 @@
+package Gitt;
+
+public class GitHello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		System.out.println("Hello...!!!!!!!");
+		System.out.println("priyanka");
+	}
+
+}
